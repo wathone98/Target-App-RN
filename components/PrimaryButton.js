f77a6377026dc793423b07bc1b6c14552642b4bc
Wa Thone <wathone98@gmail.com>
2022-09-24 15:09:12 +0800

    Feat: Add Input Number Logic

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,6 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton(props) {
-  function pressHandler() {
-    console.log("Pressed!");
-  }
+function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterCOntainer}>
       <Pressable
@@ -12,7 +9,7 @@ function PrimaryButton(props) {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        onPress={pressHandler}
+        onPress={onPress}
         android_ripple={{ color: "#490326" }}
       >
         <Text style={styles.buttonText}>{props.children}</Text>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,34 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
+  const [enteredNumber, setEnterNumber] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnterNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnterNumber('');
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      // Native Alert API
+      // Format: title, message, configured buttons of the alert
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      // Alert.prompt(); // would display dialog of the input
+      return;
+    }
+  }
+
   return (
     <View style={styles.inputCOntainer}>
       <TextInput
@@ -10,13 +37,15 @@ function StartGameScreen() {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Resut</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton style={styles.button}>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
