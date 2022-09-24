5b25ee016f90bed45c47ac9f23c485e0c756313c
Wa Thone <wathone98@gmail.com>
2022-09-24 15:22:00 +0800

    Feat: Add SafeAreaView To Add Notch On Top Of Screens

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,22 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
 function GameScreen() {
-  return <Text>Game Screen!</Text>
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text> Higher or Lower?</Text>
+      </View>
+      <View>LOG ROUNDS</View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24
+  }
+});
 
 export default GameScreen;
